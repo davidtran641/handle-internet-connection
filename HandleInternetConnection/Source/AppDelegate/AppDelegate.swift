e32f4287e7892bcad490d9d941e0a3019eedf560
davidtran <davidtran641@gmail.com>
2019-03-15 09:54:27 +0700

    Re-structure project

---
+++ HandleInternetConnection/Source/AppDelegate/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  HandleInternetConnection
+//
+//  Created by Tran Duc on 3/15/19.
+//  Copyright © 2019 David Tran. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+
+}
+
