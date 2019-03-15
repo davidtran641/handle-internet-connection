4db1e693474c154dc9bb0b065c6a93a6e1961989
davidtran <davidtran641@gmail.com>
2019-03-15 09:51:30 +0700

    Init project

---
+++ HandleInternetConnection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HandleInternetConnection
+//
+//  Created by Tran Duc on 3/15/19.
+//  Copyright © 2019 David Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
